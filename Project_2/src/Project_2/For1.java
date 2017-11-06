ea0677946f838536cc1ea320ffff216099d3559b
Minhaz01 <33205845+Minhaz01@users.noreply.github.com>
2017-11-05 21:42:29 -0600

    Update For1.java
    trying to learn Git.

--- Project_2/src/Project_2/For1.java
+++ Project_2/src/Project_2/For1.java
@@ -5,7 +5,7 @@ public class For1 {
 	public static void main(String[] args) {
 		System.out.println("Git project - 2 ");
 		System.out.println("Git project - 222222222 ");
-		
+		System.out.println("Git project - 2 and comments ");
 	}
 
 }
